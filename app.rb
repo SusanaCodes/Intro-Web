a83fd4f9b8882417bf4105098d2df765921259db
Susana <susanamncwabe@outlook.com>
2022-01-13 14:02:43 +0000

    Last commit

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/cat' do
-  @name = ["Fluffy", "Algodon", "Oscar"].sample 
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
   erb :index
 end
+
+
